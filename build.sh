f99a866a03d3706c34133b30f24d483b9c8df862
misna sadikh <misna5984@gmail.com>
2025-10-05 19:29:22 +0530

    chore: remove 61 unnecessary files - cleanup deployment configs, old migrations, and duplicate documentation

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Build script for backend
-
-pip install --upgrade pip
-cd backend && pip install -r requirements.txt
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Render start script for backend
-
-cd backend
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
